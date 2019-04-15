f888e67f760258854843d230baa5c78d97958bfa
nicolarn98 <nicola.ricchiuti@ied.edu>
2019-04-15 19:54:40 +0200

    Aggiunta configurazione al database con PDO

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,13 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        'db' =>[
+            'host' =>'127.0.0.1',
+            'dbname' => 'ticketone',
+            'port' => '3306',
+            'user' => 'root',
+            'pass' => 'root',
+        ]
     ],
 ];
